881a1f9ffd98d820dcc0a473f2858e21a4b5898b
Not-Your-Code <vineetagarwal.now@gmail.com>
2023-05-07 21:41:06 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,29 @@
+  public class first {//holder  / compose ( composite data type )
+   public static void main(String[] args) {
+  int a= 5;
+  int b= 6 ;
+
+  sum(b,a );
+
+
+  
+
+   }///
+
+
+   //function creation 
+   static void  sum(int a  , int b){
+    System.out.println(a+b);
+   
+   }   
+
+
+   // create four functions to add , sub , multiply and divide 
+
+
+// if we use anything other than void as the return type , we have to use "return" keyword;
+}
+
+
+
+
